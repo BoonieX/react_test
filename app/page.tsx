df0581d9b1df1772b8bc5ce6d9a845b7c6580e30
Reo Arya Yudha_E41231386 <2005jus@gmail.com>
2025-06-07 00:36:07 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,6 @@
 
 import { useState } from "react";
 
-function MyButton() {
-  function handleClick() {
-    alert('You clicked me!');
-  }
-
-  return (
-    <button onClick={handleClick}
-      style={{color: 'red'}}
-    >
-      Click me
-    </button>
-  );
-}
-
-function CountButton(){
-   const [count, setCount] = useState(0);
-  
-  function handleClick(){
-    setCount(count+1);
-  }
-  return (
-     <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-function SharedInfoButton({count,onClick}){
-  return(
-    <button onClick={onClick}>
-      You click {count} times
-    </button>
-  )
-}
 
 
 function ButtonImage() {
@@ -53,12 +19,10 @@ function ButtonImage() {
   );
 }
 
-const user = {
-  name: "reo",
-};
+
 
 export default function MyApp() {
-    const [count, setCount] = useState(0);
+
     
   return (
     
