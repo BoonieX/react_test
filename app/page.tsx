ba1a855c8ceedc65df6aae41494f541bb57d0353
Reo Arya Yudha_E41231386 <2005jus@gmail.com>
2025-06-07 00:47:43 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,20 @@ import { useState } from "react";
 
 
 function ButtonImage() {
-  const [image, setImage] = useState<string | null>(null);
-  
+  // Explicitly type the state as `string | undefined` (instead of `null`)
+  const [image, setImage] = useState<string | undefined>();
+
   return (
     <div>
-      <img src={image} style={{height:'500px'}}/>
-      <button onClick={() => {
-        setImage("https://www.shutterstock.com/shutterstock/photos/587857943/display_1500/stock-vector-hand-gesture-fuck-you-symbol-middle-finger-sign-cartoon-vector-illustration-sticker-587857943.jpg");
-      }} style={{border:'solid white', borderRadius:'10px',margin:'10px',padding:'10px'}}>
+      {/* Fallback to empty string if `image` is undefined */}
+      <img src={image || ""} alt="Displayed content" />
+      <button
+        onClick={() => {
+          setImage(
+            "https://www.shutterstock.com/shutterstock/photos/587857943/display_1500/stock-vector-hand-gesture-fuck-you-symbol-middle-finger-sign-cartoon-vector-illustration-sticker-587857943.jpg"
+          );
+        }}
+      >
         Press this
       </button>
     </div>
